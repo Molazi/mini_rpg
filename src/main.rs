414395708b6f5c785aca11006ef53069a9617061
Molazi <griko007@gmail.com>
2026-09-22 16:36:36 +0300

    Test bidirectional combat

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ mod enemy;
 mod player;
 
 fn main() {
-    let character_1 = player::Player::new(String::from("Arthur"), 100, 100);
+    let mut character_1 = player::Player::new(String::from("Arthur"), 100, 100);
     let mut enemy_1 = enemy::Enemy::new(String::from("Goblin"), 20, 20);
+
+    combat::attack(&enemy_1, &mut character_1, 10);
+    println!("{} HP: {}", character_1.name(), character_1.health());
 
     combat::attack(&character_1, &mut enemy_1, 20);
     println!("{} HP: {}", enemy_1.name(), enemy_1.health());

128307158626c3f2a5c250b8640f0d7519c95e98
Molazi <griko007@gmail.com>
2026-09-22 17:54:28 +0300

    Add interactive game loop

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,62 @@
 mod combat;
 mod enemy;
 mod player;
+use enemy::Enemy;
+use player::Player;
+
+enum Action {
+    Attack,
+    Heal,
+    Run,
+}
+
+fn choose_action() -> Action {
+    loop {
+        println!("Choose action: ");
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+
+        match input.trim() {
+            "1" => break Action::Attack,
+            "2" => break Action::Heal,
+            "3" => break Action::Run,
+            _ => {
+                println!("Unknown Action");
+            }
+        }
+    }
+}
 
 fn main() {
-    let mut character_1 = player::Player::new(String::from("Arthur"), 100, 100);
-    let mut enemy_1 = enemy::Enemy::new(String::from("Goblin"), 20, 20);
+    let mut player = Player::new(String::from("Arthur"), 100, 100);
+    let mut enemy_1 = Enemy::new(String::from("Goblin"), 20, 20);
 
-    combat::attack(&enemy_1, &mut character_1, 10);
-    println!("{} HP: {}", character_1.name(), character_1.health());
+    loop {
+        let action = choose_action();
+        match action {
+            Action::Attack => combat::attack(&player, &mut enemy_1, 10),
+            Action::Heal => Player::heal(&mut player, 20),
+            Action::Run => {
+                println!("Player runs away!");
+                break;
+            }
+        }
 
-    combat::attack(&character_1, &mut enemy_1, 20);
+        if enemy_1.health() == 0 {
+            println!("{} is dead!", enemy_1.name());
+            println!("You win!");
+            break;
+        }
+
+        combat::attack(&enemy_1, &mut player, 5);
+
+        if player.health() == 0 {
+            println!("{} is dead!", player.name());
+            println!("You lost!");
+            break;
+        }
+    }
+
     println!("{} HP: {}", enemy_1.name(), enemy_1.health());
+    println!("{} HP: {}", player.name(), player.health());
 }

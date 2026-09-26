108ca452962d77e0c7727cd87ac0e0d21244cb42
Molazi <griko007@gmail.com>
2026-09-26 18:17:45 +0300

    Add individual enemy damage

--- src/enemy.rs
+++ src/enemy.rs
@@ -4,17 +4,26 @@ pub struct Enemy {
     name: String,
     hp: u32,
     max_hp: u32,
+    damage: u32,
 }
 
 impl Enemy {
-    pub fn new(name: String, hp: u32, max_hp: u32) -> Enemy {
-        Enemy { name, hp, max_hp }
+    pub fn new(name: String, hp: u32, max_hp: u32, damage: u32) -> Enemy {
+        Enemy {
+            name,
+            hp,
+            max_hp,
+            damage,
+        }
     }
     pub fn health(&self) -> u32 {
         self.hp
     }
     pub fn name(&self) -> &str {
         &self.name
+    }
+    pub fn attack_damage(&self) -> u32 {
+        self.damage
     }
     pub fn take_damage(&mut self, damage: u32) {
         if damage >= self.hp {
@@ -49,24 +58,29 @@ impl Damageable for Enemy {
 mod tests {
     #[test]
     fn test_take_damage() {
-        let mut enemy_1 = super::Enemy::new(String::from("Goblin"), 20, 20);
+        let mut enemy_1 = super::Enemy::new(String::from("Goblin"), 20, 20, 10);
         enemy_1.take_damage(10);
         assert_eq!(enemy_1.health(), 10);
     }
     #[test]
     fn test_heal() {
-        let mut enemy_1 = super::Enemy::new(String::from("Goblin"), 10, 20);
+        let mut enemy_1 = super::Enemy::new(String::from("Goblin"), 10, 20, 10);
         enemy_1.heal(20);
         assert_eq!(enemy_1.health(), enemy_1.max_hp);
     }
     #[test]
     fn test_is_alive() {
-        let enemy = super::Enemy::new(String::from("Arthur"), 100, 100);
+        let enemy = super::Enemy::new(String::from("Goblin"), 100, 100, 10);
         assert!(enemy.is_alive());
     }
     #[test]
     fn test_is_alive_2() {
-        let enemy = super::Enemy::new(String::from("Arthur"), 0, 100);
+        let enemy = super::Enemy::new(String::from("Goblin"), 0, 100, 10);
         assert!(!enemy.is_alive());
+    }
+    #[test]
+    fn test_attack_damage() {
+        let enemy = super::Enemy::new(String::from("Goblin"), 20, 20, 5);
+        assert_eq!(enemy.attack_damage(), 5);
     }
 }
